a2d1999f0d5a2b03d4b02e484a2b824da1cb5631
Gabriel Victor <gabriel.victor.ti@gmail.com>
2018-10-01 10:03:58 -0300

    Adicionado mais 2 registros na tabela Categorias

--- src/main/java/com/gabrielvictor/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/gabrielvictor/cursomc/resources/CategoriaResource.java
@@ -16,10 +16,14 @@ public class CategoriaResource {
 	public List<Categoria> listar() {
 		Categoria cat1 = new Categoria(1, "Informática");
 		Categoria cat2 = new Categoria(2, "Escritório");
+		Categoria cat3 = new Categoria(3, "Decoração");
+		Categoria cat4 = new Categoria(4, "Infantil");
 		
 		List<Categoria> lista = new ArrayList<>();
 		lista.add(cat1);
 		lista.add(cat2);
+		lista.add(cat3);
+		lista.add(cat4);
 		return lista;
 	}
 }
